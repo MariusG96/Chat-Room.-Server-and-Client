061a1be751ded2b7c24d66685d51ba05a7f0fed3
MariusG96 <54101353+MariusG96@users.noreply.github.com>
2019-08-14 10:30:48 +0100

    Add files via upload

---
+++ ChatRoomServer/ChatRoomServer/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace ChatRoomServer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PortTextBox = new System.Windows.Forms.TextBox();
+            this.StartButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.StopButton = new System.Windows.Forms.Button();
+            this.DisplayTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // PortTextBox
+            // 
+            this.PortTextBox.Location = new System.Drawing.Point(41, 466);
+            this.PortTextBox.Name = "PortTextBox";
+            this.PortTextBox.Size = new System.Drawing.Size(100, 20);
+            this.PortTextBox.TabIndex = 2;
+            // 
+            // StartButton
+            // 
+            this.StartButton.Location = new System.Drawing.Point(12, 492);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(75, 23);
+            this.StartButton.TabIndex = 3;
+            this.StartButton.Text = "Start";
+            this.StartButton.UseVisualStyleBackColor = true;
+            this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 469);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Port";
+            // 
+            // StopButton
+            // 
+            this.StopButton.Location = new System.Drawing.Point(93, 492);
+            this.StopButton.Name = "StopButton";
+            this.StopButton.Size = new System.Drawing.Size(75, 23);
+            this.StopButton.TabIndex = 5;
+            this.StopButton.Text = "Stop";
+            this.StopButton.UseVisualStyleBackColor = true;
+            this.StopButton.Click += new System.EventHandler(this.StopButton_Click);
+            // 
+            // DisplayTextBox
+            // 
+            this.DisplayTextBox.Location = new System.Drawing.Point(11, 11);
+            this.DisplayTextBox.Multiline = true;
+            this.DisplayTextBox.Name = "DisplayTextBox";
+            this.DisplayTextBox.Size = new System.Drawing.Size(531, 446);
+            this.DisplayTextBox.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(551, 527);
+            this.Controls.Add(this.DisplayTextBox);
+            this.Controls.Add(this.StopButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.StartButton);
+            this.Controls.Add(this.PortTextBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox PortTextBox;
+        private System.Windows.Forms.Button StartButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button StopButton;
+        private System.Windows.Forms.TextBox DisplayTextBox;
+    }
+}
+
---
+++ ChatRoomServer/ChatRoomServer/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace ChatRoomServer
+{
+    public partial class Form1 : Form
+    {
+        TcpListener listener;
+        TcpClient client;
+        Thread thread;
+        object hum = new object();
+        Dictionary<int, TcpClient> list_clients = new Dictionary<int, TcpClient>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            CheckForIllegalCrossThreadCalls = false;         
+        }
+
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            int port = int.Parse(PortTextBox.Text);
+
+            Listen(port);
+        }
+
+        private void Listen(int port)
+        {
+            int count = 1;
+
+            // Listen for input coming from any IP address on specified port
+            listener = new TcpListener(IPAddress.Any, port);
+            listener.Start();
+            DisplayTextBox.AppendText("Server Running" + Environment.NewLine);
+
+            try
+            { 
+                while (true)
+                {
+                    // Create an event handler for dealing with incoming connections
+                    client = listener.AcceptTcpClient();
+                    lock (hum) list_clients.Add(count, client);
+
+
+                    thread = new Thread(receiver);
+                    thread.Start(count);
+                    count++;
+                }
+            }
+            catch(Exception) { }
+            //For some reason it kind of crashes but it continues to run still receiving users, broadcasting messages, etc......
+        }
+
+        private void receiver(object o)
+        {
+
+            int id = (int)o;
+            TcpClient client;
+            lock (hum) client = list_clients[id];
+           
+            try
+            { 
+                while (true)
+                {
+                    NetworkStream stream = client.GetStream();
+                    byte[] buffer = new byte[1024];
+                    int byte_count = stream.Read(buffer, 0, buffer.Length);
+
+                    if (byte_count == 0)
+                    {
+                        break;
+                    }
+
+                    string data = Encoding.ASCII.GetString(buffer, 0, byte_count);
+                    //DisplayTextBox.AppendText(data + Environment.NewLine);
+                    broadcast(data);
+                    
+                }
+            }
+            catch(Exception) { }
+
+            lock (hum) list_clients.Remove(id);
+            client.Client.Shutdown(SocketShutdown.Both);
+            client.Close();
+        }
+
+        private void broadcast(string data)
+        {
+            byte[] buffer = Encoding.ASCII.GetBytes(data + Environment.NewLine);
+
+            lock (hum)
+            {
+                foreach (TcpClient c in list_clients.Values)
+                {
+                    NetworkStream stream = c.GetStream();
+
+                    stream.Write(buffer, 0, buffer.Length);
+                }
+            }
+        }
+        private void StopButton_Click(object sender, EventArgs e)
+        {
+            listener.Stop();
+        }
+
+
+    }
+}
